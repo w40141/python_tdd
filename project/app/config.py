c4cfd5850e0e6d5031ea84f37a2fb589083e6f0d
w40141 <w.40141@gmail.com>
2023-04-07 10:08:08 +0900

    feat: done 'Docker Config'

---
+++ project/app/config.py
@@ -0,0 +1,18 @@
+import logging
+from functools import lru_cache
+
+from pydantic import AnyUrl, BaseSettings
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = bool(0)
+    database_url: AnyUrl = None
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
