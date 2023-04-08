a1216a2d26bea9f96acfc8bbb34b011d8758ba54
w40141 <w.40141@gmail.com>
2023-04-08 12:52:38 +0900

    feat: pdm

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -10,4 +10,4 @@ echo "PostgreSQL started"
 
 exec "$@"
 
-cd /usr/src/app && uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
+# cd /usr/src/app && uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
