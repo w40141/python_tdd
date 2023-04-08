08cdc38791600786a8a044a4777911f903bd05ee
w40141 <w.40141@gmail.com>
2023-04-09 01:31:11 +0900

    db

--- scripts/endpoint.sh
+++ scripts/endpoint.sh
@@ -1,2 +1,14 @@
-cd /project && pdm run uvicorn app.main:app --reload --port=8000 --host=0.0.0.0
-echo "Start"
+#!/bin/sh
+
+# cd /project && pdm run uvicorn app.main:app --reload --port=8000 --host=0.0.0.0
+# echo "Start"
+
+echo "Waiting for postgres..."
+
+while ! nc -z web-db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+exec "$@"
